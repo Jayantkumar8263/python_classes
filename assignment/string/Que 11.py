92ec9ced9252abf5edb8300adcd3b3b1e53386a9
Jayantkumar8263 <pjayantk@gmail.com>
2024-05-26 10:55:11 +0530

    26.01.24

--- assignment/string/Que 11.py
+++ assignment/string/Que 11.py
@@ -1,4 +1,5 @@
 'Write a Python program to remove characters that have odd index values in a given string.'
 a = ("abcdefghij")
-for i in a :
-    if i % 2 != 0:
+#for i in a :
+print(a[1], a[3], a[5], a[7], a[9])
+            
